b0711332541a3bb81060e5afb0df017da71218b7
JoshPattani <jopa8577@colorado.edu>
2025-02-10 12:06:35 -0700

    updated code and libs

---
+++ iot_api_node_osc_maxpat/iot_api_node.js
@@ -0,0 +1,230 @@
+// Require built-in modules
+const https = require("https");
+const dgram = require("dgram");
+const maxApi = require("max-api");
+
+// Require third-party modules
+const { ArduinoIoTCloud } = require("arduino-iot-js");
+const ArduinoIotClient = require("@arduino/arduino-iot-client");
+const fetch = require("node-fetch");
+
+// ====================
+// CONFIGURATION
+// ====================
+
+// Arduino IoT Cloud API endpoint.
+// Replace YOUR_THING_ID with your actual Thing ID and adjust the URL as needed.
+const API_URL = "https://api2.arduino.cc/iot/v1/clients/token";
+
+// Replace with your actual API token.
+const CLIENT_ID = "XUg1anbdoKyu5tHu4AL0CqobAKsl9CN5";
+const CLIENT_SECRET =
+ "bOucUKZewfYnxkbPuItzVuZ0JXV61GJUpgQAAiHH7jCwPYjAN9rbxODhWhxHcl5m";
+const THING_ID = "9f817909-9317-4b6f-82dd-02620591f54b";
+const VARIABLE_ID = "9d55b47c-8b95-42b6-8634-2c24bb614fa0";
+
+// Polling interval in milliseconds
+const POLL_INTERVAL = 1000; // 1 second
+
+// OSC destination (e.g., where a udpreceive object in your patch is listening)
+const OSC_IP = "192.168.0.8";
+const OSC_PORT = 8000;
+
+// Create a UDP socket (IPv4)
+const socket = dgram.createSocket("udp4");
+
+// ====================
+// HELPER FUNCTIONS
+// ====================
+
+/**
+ * Gets an access token from the Arduino IoT Cloud API.
+ * The token is stored in the global accessToken variable.
+ */
+async function getAccessToken() {
+ try {
+  const response = await fetch(API_URL, {
+   method: "POST",
+   url: API_URL,
+   headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+   },
+   form: {
+    grant_type: "client_credentials",
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    audience: "https://api2.arduino.cc/iot",
+   },
+  });
+
+  const data = await response.json();
+  return data["access_token"];
+ } catch (error) {
+  maxApi.post(`Error obtaining access token: ${error.message}\n`);
+ } finally {
+  maxApi.post("Access token obtained successfully.\n");
+ }
+}
+
+/**
+ * Connects to the Arduino IoT Client API.
+ * Listens to a thing's property changes.
+ */
+async function connectToArduinoIoTClient() {
+ // Get the access token
+ var client = ArduinoIotClient.ApiClient.instance;
+ // Configure OAuth2 access token for authorization: oauth2
+ var oauth2 = client.authentications["oauth2"];
+ oauth2.accessToken = await getAccessToken(); // Changed to getAccessToken() function
+ // Create an instance of the API class
+ var apiInstance = new ArduinoIotClient.ThingsV2Api(client);
+ var thingId = THING_ID; // String | The id of the thing
+ var propertyId = VARIABLE_ID; // String | The id of the property
+ var opts = {
+  onMessage: function (data, response) {
+   // Callback function that is called when a message is received
+   console.log("onMessage called");
+   console.log("Data: " + data);
+   console.log("Response: " + response);
+  },
+ };
+}
+
+/**
+ * Connects to the Arduino IoT Cloud API.
+ * Listens to a thing's property changes.
+ *
+ */
+async function connectToArduinoIoTCloud() {
+ // Connect to the Arduino IoT Cloud API
+ maxApi.post("Connecting to Arduino IoT Cloud API...\n");
+ try {
+  const client = await ArduinoIoTCloud.connect({
+   clientId: CLIENT_ID,
+   clientSecret: CLIENT_SECRET,
+   onDisconnect: (message) => {
+    maxApi.post("Disconnected from Arduino IoT Cloud: " + message + "\n");
+    console.error(message);
+   },
+  }).then(() => {
+   maxApi.post("Connected to Arduino IoT Cloud API.\n");
+   return ArduinoIoTCloud.onPropertyValue(THING_ID, VARIABLE_ID, (value) => {
+    processReceivedValue(value);
+   });
+  });
+ } catch (error) {
+  maxApi.post(`Error connecting to Arduino IoT Cloud: ${error.message}\n`);
+ }
+}
+
+/**
+ * Processes the received value.
+ *
+ * @param {number} value - The received value.
+ */
+function processReceivedValue(value) {
+ // Create an OSC message with the address "/sensor".
+ const oscMsg = createOscMessage("/sensor", value);
+
+ maxApi.post("OSC Message created \n");
+
+ // Send the OSC message via UDP.
+ socket.send(oscMsg, 0, oscMsg.length, OSC_PORT, OSC_IP, (err) => {
+  if (err) {
+   maxApi.post("Error sending OSC message: " + err + "\n");
+  } else {
+   maxApi.post("Sent sensor value: " + value + "\n");
+  }
+ });
+}
+
+/**
+ * Pads a string (with a trailing null) so its length is a multiple of 4.
+ * OSC strings must be null-terminated and padded with zeros.
+ *
+ * @param {string} str - The string to pad.
+ * @returns {Buffer} - The padded string as a Buffer.
+ */
+function padString(str) {
+ // Add a null terminator
+ let buf = Buffer.from(str + "\0");
+ // Compute the number of zero bytes needed to reach a multiple of 4.
+ const padBytes = (4 - (buf.length % 4)) % 4;
+ if (padBytes > 0) {
+  const pad = Buffer.alloc(padBytes, 0);
+  buf = Buffer.concat([buf, pad]);
+ }
+ return buf;
+}
+
+/**
+ * Creates a simple OSC message for an integer value.
+ *
+ * OSC message format:
+ *   - OSC Address (e.g., "/sensor")
+ *   - Type Tag String (e.g., ",i" for a 32-bit integer)
+ *   - 32-bit big-endian integer value
+ *
+ * @param {string} address - The OSC address.
+ * @param {number} value - The sensor value.
+ * @returns {Buffer} - The complete OSC message.
+ */
+function createOscMessage(address, value) {
+ // Create the padded OSC address.
+ const addressBuf = padString(address);
+ // Create the padded type tag. (Comma + "i" means one integer argument.)
+ const typeTagBuf = padString(",i");
+ // Allocate a 4-byte buffer for the integer value.
+ const valueBuf = Buffer.alloc(4);
+ valueBuf.writeInt32BE(value, 0);
+
+ // Concatenate all parts into one Buffer.
+ return Buffer.concat([addressBuf, typeTagBuf, valueBuf]);
+}
+
+/**
+ * Polls the Arduino IoT Cloud API for the sensor value.
+ * On success, builds an OSC message and sends it via UDP.
+ */
+async function pollApi() {
+ // Connect to the Arduino IoT Cloud API
+ await connectToArduinoIoTCloud();
+ //  await connectToArduinoIoTClient();
+
+ //  const options = {
+ //   uri: `https://api.arduino.cc/iot/v1/things/${THING_ID}/properties/${VARIABLE_ID}/value`,
+ //   method: "GET",
+ //   json: true,
+ //   headers: {
+ //    Authorization: `Bearer ${await getAccessToken()}`,
+ //   },
+ //  };
+
+ //  try {
+ //   const response = await fetch(options.uri, {
+ //    method: options.method,
+ //    headers: options.headers,
+ //   });
+
+ //   const data = await response.json();
+ //   const sensorValue = data.value;
+ //   processReceivedValue(sensorValue);
+ //  } catch (error) {
+ //   maxApi.post(`Error polling API: ${error.message}\n`);
+ //  }
+}
+
+// ====================
+// MAIN
+// ====================
+// Start polling the API
+pollApi();
+
+// Optionally, you can expose functions to MAX messages if desired.
+// For example, to force a poll on a "bang" from MAX, you could define:
+//
+// exports.bang = function() {
+//     pollApi();
+// };
+//
+// In your MAX patch, sending a "bang" to the node.script object will call pollApi().
---
+++ iot_api_node_osc_maxpat/iot_api_node_osc.maxpat
@@ -0,0 +1,266 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 9,
+			"minor" : 0,
+			"revision" : 3,
+			"architecture" : "x64",
+			"modernui" : 1
+		}
+,
+		"classnamespace" : "box",
+		"rect" : [ 34.0, 76.0, 1069.0, 755.0 ],
+		"gridsize" : [ 15.0, 15.0 ],
+		"boxes" : [ 			{
+				"box" : 				{
+					"id" : "obj-11",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 307.352935314178467, 88.970586538314819, 217.0, 22.0 ],
+					"text" : "script npm i @arduino/arduino-iot-client"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-7",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 289.0, 51.0, 269.0, 22.0 ],
+					"text" : "script npm install --save @types/request-promise"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-5",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 256.0, 27.0, 171.0, 22.0 ],
+					"text" : "script npm install arduino-iot-js"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-17",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 183.0, 27.0, 63.0, 22.0 ],
+					"text" : "script stop"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-16",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 99.0, 27.0, 64.0, 22.0 ],
+					"text" : "script start"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"bgmode" : 0,
+					"border" : 0,
+					"clickthrough" : 0,
+					"enablehscroll" : 0,
+					"enablevscroll" : 0,
+					"id" : "obj-13",
+					"lockeddragscroll" : 0,
+					"lockedsize" : 0,
+					"maxclass" : "bpatcher",
+					"name" : "n4m.monitor.maxpat",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"offset" : [ 0.0, 0.0 ],
+					"outlettype" : [ "bang" ],
+					"patching_rect" : [ 99.0, 137.0, 400.0, 220.0 ],
+					"viewvisibility" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-10",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 673.0, 183.0, 50.0, 22.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-9",
+					"maxclass" : "newobj",
+					"numinlets" : 2,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 673.0, 82.0, 79.0, 22.0 ],
+					"text" : "route /sensor"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-4",
+					"maxclass" : "button",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 600.0, 150.0, 24.0, 24.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-2",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 600.0, 27.0, 97.0, 22.0 ],
+					"text" : "udpreceive 8000"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 99.0, 82.0, 151.0, 22.0 ],
+					"saved_object_attributes" : 					{
+						"autostart" : 0,
+						"defer" : 0,
+						"watch" : 0
+					}
+,
+					"text" : "node.script iot_api_node.js",
+					"textfile" : 					{
+						"filename" : "iot_api_node.js",
+						"flags" : 0,
+						"embed" : 0,
+						"autowatch" : 1
+					}
+
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-13", 0 ],
+					"source" : [ "obj-1", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"midpoints" : [ 316.852935314178467, 111.0, 261.0, 111.0, 261.0, 69.0, 108.5, 69.0 ],
+					"source" : [ "obj-11", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-16", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-17", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"order" : 1,
+					"source" : [ "obj-2", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-9", 0 ],
+					"order" : 0,
+					"source" : [ "obj-2", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-5", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-7", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-10", 0 ],
+					"source" : [ "obj-9", 0 ]
+				}
+
+			}
+ ],
+		"originid" : "pat-6",
+		"dependency_cache" : [ 			{
+				"name" : "fit_jweb_to_bounds.js",
+				"bootpath" : "C74:/packages/Node for Max/patchers/debug-monitor",
+				"type" : "TEXT",
+				"implicit" : 1
+			}
+, 			{
+				"name" : "iot_api_node.js",
+				"bootpath" : "~/ReSi_Rendered-Embodiment-of-Social-Interactions/iot_api_node_osc_maxpat",
+				"patcherrelativepath" : ".",
+				"type" : "TEXT",
+				"implicit" : 1
+			}
+, 			{
+				"name" : "n4m.monitor.maxpat",
+				"bootpath" : "C74:/packages/Node for Max/patchers/debug-monitor",
+				"type" : "JSON",
+				"implicit" : 1
+			}
+, 			{
+				"name" : "resize_n4m_monitor_patcher.js",
+				"bootpath" : "C74:/packages/Node for Max/patchers/debug-monitor",
+				"type" : "TEXT",
+				"implicit" : 1
+			}
+ ],
+		"autosave" : 0
+	}
+
+}
