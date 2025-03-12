02d1994d5fc40a9148572c927144debd79edcf97
JoshPattani <jopa8577@colorado.edu>
2025-03-11 20:34:46 -0600

    Update 3D models; enhance OSC configuration and LSL data processing for Focus and Relaxation metrics

--- brainwaveStreaming/lsl-to-osc-bridge/src/config.py
+++ brainwaveStreaming/lsl-to-osc-bridge/src/config.py
@@ -2,7 +2,11 @@
 
 LSL_SOURCE_NAME = "YourLSLSourceName"
 LSL_DATA_TYPE = "YourDataType"
-OSC_IP = "127.0.0.1"
-OSC_PORT = 8000
+OSC_IP = "10.201.78.68"  # UCB # local: "127.0.0.1"
+OSC_PORT = 42069
 OSC_ADDRESS = "/your/osc/address"
 DATA_SEND_INTERVAL = 0.01  # Interval in seconds for sending data
+SCALE_FACTOR = 1.0  # Scale factor for EEG data
+SAMPLE_RATE = 250  # Sampling rate of the data
+DEBUG_MODE = False  # Set to False in production
+IGNORE_QUALITY_CHECKS = False  # Set to False in production
